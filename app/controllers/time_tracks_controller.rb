de7eb45cfa72e2f87a4260d95717cd4518725bc3
AdrianManteza <admanteza@mail.com>
2019-03-09 08:51:43 -0500

    time track controller specs

--- app/controllers/time_tracks_controller.rb
+++ app/controllers/time_tracks_controller.rb
@@ -1,4 +1,24 @@
 class TimeTracksController < ApplicationController
+  before_action :teacher, only: [:create]
+
   def new
+    @time_track = TimeTrack.new
+  end
+
+  def create
+  end
+
+private
+
+  def teacher
+    @teacher ||= Teacher.find(time_track_params[:teacher_id])
+  end
+
+  def time_track_params
+    params.fetch(:time_track).permit(
+      :teacher_id,
+      :clock_in,
+      :clock_out
+    )
   end
 end
--- spec/controllers/time_tracks_controller_spec.rb
+++ spec/controllers/time_tracks_controller_spec.rb
@@ -6,6 +6,25 @@ RSpec.describe TimeTracksController do
       get :new
 
       expect(response).to render_template(:new)
+      expect(assigns(:time_track)).to be_truthy
+    end
+  end
+
+  describe '#create' do
+    let(:time_track_params) do
+      {
+        time_track: {
+          teacher_id: 1,
+          clock_in: Time.now,
+          clock_out: nil
+        }
+      }
+    end
+
+    it 'creates a new Time Track for a Teacher' do
+      expect do
+        post :create, params: time_track_params
+      end.to change(TimeTrack, :count).by(1)
     end
   end
 end
